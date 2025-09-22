7fc6f8550d3722ec7f7471339119377025e955c0
Val Lysenko <kakao757@gmail.com>
2025-09-22 16:57:31 -0700

    update Animals page

--- client/src/pages/Animals.tsx
+++ client/src/pages/Animals.tsx
@@ -17,10 +17,6 @@ const Animals: FC = () => {
 
   return (
     <div className="flex flex-col w-full min-h-screen">
-      <section className="h-[70vh] w-full">
-        <Header />
-      </section>
-
       <main className="flex-1 w-full">
         <div className="container mx-auto px-6 py-8">
           <h2 className="text-3xl font-bold text-center mb-8 text-gray-800">
