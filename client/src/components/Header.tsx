ac0158c21adde3dc6d80e184f80cd4908dd87898
rubenaguilardev <ragui56@wgu.edu>
2025-09-22 02:03:57 -0700

    add logo, rename nav links

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,17 +1,24 @@
 import type { FC } from "react";
 import { NavLink } from "react-router-dom";
+import logo  from "../assets/header/logo.png"
 
 const Header: FC = () => {
   return (
-    <header className="w-full h-[40vh] bg-gray-800 text-white flex flex-col items-center justify-center">
-      <div className="w-full">
-        <h1 className="text-5xl text-center mb-8">The Animal Shelter App</h1>
+    <header className="flex justify-between items-center text-black px-12 py-9 border border-dashed">
+      <div>
+        <img 
+          src={logo} 
+          alt="Little Paws logo"
+          className="w-[295px]"
+        />
+      </div>
+      <div>
         <nav className="flex justify-center space-x-8">
           <NavLink
             to="/"
             className={({ isActive }: { isActive: boolean }) =>
               `text-lg hover:text-blue-300 transition-colors ${
-                isActive ? "text-blue-400 font-semibold" : "text-white"
+                isActive ? "text-blue-400 font-semibold" : "text-green"
               }`
             }
           >
@@ -21,17 +28,27 @@ const Header: FC = () => {
             to="/animals"
             className={({ isActive }: { isActive: boolean }) =>
               `text-lg hover:text-blue-300 transition-colors ${
-                isActive ? "text-blue-400 font-semibold" : "text-white"
+                isActive ? "text-blue-400 font-semibold" : "text-green"
               }`
             }
           >
-            Animals
+            Dogs
           </NavLink>
           <NavLink
             to="/adopt-vs-foster"
             className={({ isActive }: { isActive: boolean }) =>
               `text-lg hover:text-blue-300 transition-colors ${
-                isActive ? "text-blue-400 font-semibold" : "text-white"
+                isActive ? "text-blue-400 font-semibold" : "text-green"
+              }`
+            }
+          >
+            Cats
+          </NavLink>
+          <NavLink
+            to="/adopt-vs-foster"
+            className={({ isActive }: { isActive: boolean }) =>
+              `text-lg hover:text-blue-300 transition-colors ${
+                isActive ? "text-blue-400 font-semibold" : "text-green"
               }`
             }
           >
