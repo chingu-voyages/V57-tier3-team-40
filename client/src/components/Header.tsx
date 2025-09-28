6b9093c868cec96293804bac1c516bc53338da20
rubenaguilardev <ragui56@wgu.edu>
2025-09-28 02:40:11 -0700

    update color

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,25 +5,25 @@ import { FaAngleDown, FaAngleUp } from 'react-icons/fa'
 
 const Header: FC = () => {
   return (
-     <header className="bg-white">
-      <div className="max-w-[1440px] w-full mx-auto flex justify-between items-center text-black px-12 py-6">
+     <header className="border border-dashed">
+      <div className="max-w-[1440px] w-full mx-auto flex justify-between items-center text-black px-12 py-5">
         <div>
           <NavLink to='/'>
             <img 
               src={headerLogo} 
               alt="Little Paws logo"
-              className="w-[240px]"
+              className="w-[230px]"
             />
           </NavLink>
         </div>
         <div>
-          <nav className="flex justify-center space-x-13">
+          <nav className="flex justify-center space-x-12">
             {navInfo.map((link, index)=> (
               <NavLink
                 key={index}
                 to={link.to}
                 className={({ isActive }: { isActive: boolean }) =>
-                  `text-lg font-medium hover:text-[#08872B]/60 transition-colors ${
+                  `font-medium hover:text-[#08872B]/60 transition-colors ${
                     isActive ? "text-[#08872B]" : "text-[#2D3142]"
                   }`
                 }
